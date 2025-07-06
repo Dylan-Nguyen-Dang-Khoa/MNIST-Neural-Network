ff14a226cb87566e6373593a787ca44afd20c691
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-07-06 22:10:03 +0800

    Made the layer class which is the object of one layer in the neural network

---
+++ Neural_Network.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class Layer:
+    def __init__(self, weights, bias):
+        self.weights = weights  # The weight matrix is of the order n_out * n_in
+        self.bias = bias  # The bias vector is a column vector with n_out biases
+
+    def forward_pass(self, input):  # Input is a column vector
+        output = np.dot(self.weights, input) + self.bias
