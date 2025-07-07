d8229ec032f214f70d422f7f87dad45af2af49b1
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-07-07 16:46:39 +0800

    Added in the two activation functions ReLu and softmax for the layers in the NN and also initialised the NN layers

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,14 +1,34 @@
 import numpy as np
 
+def he_normal(fan_in, fan_out):
+    return np.random.normal(loc=0.0, scale=np.sqrt(2/fan_in), size=(fan_out, fan_in))
 
 class Layer:
-    def __init__(self, weights, bias):
+    def __init__(self, weights, bias, activation):
         self.weights = weights  # The weight matrix is of the order n_out * n_in
         self.bias = bias  # The bias vector is a column vector with n_out biases
+        self.activation = activation
 
     def forward_pass(self, input):  # Input is a column vector
-        output = np.dot(self.weights, input) + self.bias  # Equivalent to z = Wx + b
-        return max(
-            0, output
-        )  # Implemented ReLu activation for the pre-activation output
+        pre_activation_output = np.dot(self.weights, input) + self.bias  # Equivalent to z = Wx + b
+        return self.ReLu(pre_activation_output) if self.activation == "relu" else self.softmax(pre_activation_output) # Return activated output with ReLu if hidden layer else use softmax for output layer
+        
+    def ReLu(self, pre_activation_output):
+        return np.maximum(0, pre_activation_output)
+    
+    def softmax(self, pre_activation_output):
+        z_shifted = pre_activation_output - np.max(pre_activation_output, axis=0, keepdims=True)
+        z_exp = np.exp(z_shifted)
+        return z_exp/np.sum(z_exp, axis=0, keepdims=True)
+    
+
+
+
+def main():
+    hidden_layer_1 = Layer(he_normal(784, 512), np.zeros((512, 1)), "relu")
+    hidden_layer_2 = Layer(he_normal(512, 256), np.zeros((256, 1)), "relu")
+    hidden_layer_3 = Layer(he_normal(256, 128), np.zeros((128, 1)), "relu")
+    output_layer = Layer(he_normal(128, 10), np.zeros((10, 1)), "softmax")
+
+
 
