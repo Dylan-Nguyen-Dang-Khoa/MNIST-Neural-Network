91a9dac8850be183f1bf381a48abea17f7e376d4
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-07-10 20:07:52 +0800

    Reformatted the bias to be a 1D vector to prevent weird behaviour and outputs

--- Neural_Network.py
+++ Neural_Network.py
@@ -35,20 +35,25 @@ def he_normal(fan_in, fan_out):
 def load_data():
     data = np.loadtxt("mnist_train.csv", delimiter=",", skiprows=1)
     X_data, Y_data = shuffle_data(data[:, 1:], data[:, 0].astype(int))
-    return X_data[:50000, :], Y_data[:50000, :], X_data[50000:, :], Y_data[50000:, :]
+    return (
+        X_data[:50000, :] / 255,
+        Y_data[:50000],
+        X_data[50000:, :] / 255,
+        Y_data[50000:],
+    )
 
 
 def shuffle_data(x_data, y_data):
-    assert x_data[0] == y_data[0]
-    permuted_indicies = np.random.permutation(x_data[0])
+    assert x_data.shape[0] == y_data.shape[0]
+    permuted_indicies = np.random.permutation(x_data.shape[0])
     return x_data[permuted_indicies], y_data[permuted_indicies]
 
 
 def forward_propagation(X_train, Y_train):
-    hidden_layer_1 = Layer(he_normal(784, 512), np.zeros((512, 1)), "relu")
-    hidden_layer_2 = Layer(he_normal(512, 256), np.zeros((256, 1)), "relu")
-    hidden_layer_3 = Layer(he_normal(256, 128), np.zeros((128, 1)), "relu")
-    output_layer = Layer(he_normal(128, 10), np.zeros((10, 1)), "softmax")
+    hidden_layer_1 = Layer(he_normal(784, 512), np.zeros((512,)), "relu")
+    hidden_layer_2 = Layer(he_normal(512, 256), np.zeros((256,)), "relu")
+    hidden_layer_3 = Layer(he_normal(256, 128), np.zeros((128,)), "relu")
+    output_layer = Layer(he_normal(128, 10), np.zeros((10,)), "softmax")
     max_epochs = 80
     correct_outputs = 0
     total_outputs = 0
@@ -57,8 +62,13 @@ def forward_propagation(X_train, Y_train):
             layer_1_output = hidden_layer_1.forward_pass(X_train[row, :])
             layer_2_output = hidden_layer_2.forward_pass(layer_1_output)
             layer_3_output = hidden_layer_3.forward_pass(layer_2_output)
+            final_output = output_layer.forward_pass(layer_3_output)
+            print(final_output)
 
 
 def main():
     X_train, Y_train, X_validation, Y_validation = load_data()
     forward_propagation(X_train, Y_train)
+
+
+main()
