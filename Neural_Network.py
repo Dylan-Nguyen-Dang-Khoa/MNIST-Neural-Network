be9c0a51a5d41a7a69b27eacc39b2a8250b22ffb
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-07-14 09:55:25 +0800

    Refactored the entire codebase. Made a load data class for loading data, a network class for all the activities regarding the network. Kept the layer class to be used in the network class. Moved the relevant functions to the relevant classes

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,21 +1,66 @@
 import numpy as np
 
 
-class Layer:
-    def __init__(self, weights, bias, activation):
-        self.weights = weights  # The weight matrix is of the order n_out * n_in
-        self.bias = bias  # The bias vector is a column vector with n_out biases
-        self.activation = activation
+class LoadData:
+    def __init__(self):
+        training_data = np.loadtxt("mnist_train.csv", delimiter=",", skiprows=1)
+        X_train, Y_train = self.shuffle_data(
+            training_data[:, 1:], training_data[:, 0].astype(int)
+        )
+        test_data = np.loadtxt("mnist_test.csv", delimiter=",", skiprows=1)
+        X_test, Y_test = self.shuffle_data(
+            test_data[:, 1:], test_data[:, 0].astype(int)
+        )
 
-    def forward_pass(self, input):  # Input is a column vector
-        pre_activation_output = (
-            np.dot(self.weights, input) + self.bias
-        )  # Equivalent to z = Wx + b
+    def shuffle_data(self):
+        assert self.X_data.shape[0] == self.Y_data.shape[0]
+        permuted_indicies = np.random.permutation(self.X_data.shape[0])
+        return self.X_data[permuted_indicies], self.Y_data[permuted_indicies]
+
+    def load_training_data(self):
         return (
-            self.ReLu(pre_activation_output)
-            if self.activation == "relu"
-            else self.softmax(pre_activation_output)
-        )  # Return activated output with ReLu if hidden layer else use softmax for output layer
+            self.X_data[:50000, :] / 255,
+            self.Y_data[:50000],
+        )
+
+    def load_validation_data(self):
+        return self.X_data[50000:, :] / 255, self.Y_data[50000:]
+
+    def load_test_data(self):
+        return self.X_test, self.Y_test
+
+
+class Layer:
+    def __init__(self, inputs, outputs):
+        self.weights = self.he_normal(
+            inputs, outputs
+        )  # The weight matrix is of the order n_out * n_in
+        self.bias = np.zeros(
+            outputs
+        )  # The bias vector is a column vector with n_out biases
+
+    def he_normal(self, fan_in, fan_out):
+        return np.random.normal(
+            loc=0.0, scale=np.sqrt(2 / fan_in), size=(fan_out, fan_in)
+        )
+
+
+class Network:
+    def __init__(self, layers):
+        self.l1 = Layer(784, 512)
+        self.l2 = Layer(512, 256)
+        self.l3 = Layer(256, 128)
+        self.l4 = Layer(128, 10)
+
+    def dropout_mask(self, activated_output, keep_prob):
+        mask = np.random.rand(*activated_output.shape) < keep_prob
+        dropped_output = activated_output * mask
+        return dropped_output
+
+    def forward_propagation(self, data):
+        self.z1 = self.l1.weights @ data + self.l1.bias
+        self.a1 = self.ReLu(self.z1)
+        self.d1 = self.dropout_mask(self.a1, 0.7)
 
     def ReLu(self, pre_activation_output):
         return np.maximum(0, pre_activation_output), pre_activation_output
@@ -28,46 +73,11 @@ class Layer:
         return z_exp / np.sum(z_exp, axis=0, keepdims=True), pre_activation_output
 
 
-def he_normal(fan_in, fan_out):
-    return np.random.normal(loc=0.0, scale=np.sqrt(2 / fan_in), size=(fan_out, fan_in))
-
-
-def load_data():
-    data = np.loadtxt("mnist_train.csv", delimiter=",", skiprows=1)
-    X_data, Y_data = shuffle_data(data[:, 1:], data[:, 0].astype(int))
-    return (
-        X_data[:50000, :] / 255,
-        Y_data[:50000],
-        X_data[50000:, :] / 255,
-        Y_data[50000:],
-    )
-
-
-def shuffle_data(x_data, y_data):
-    assert x_data.shape[0] == y_data.shape[0]
-    permuted_indicies = np.random.permutation(x_data.shape[0])
-    return x_data[permuted_indicies], y_data[permuted_indicies]
-
-
-def dropout_mask(activated_output, keep_prob):
-    mask = np.random.rand(*activated_output.shape) < keep_prob
-    dropped_output = activated_output * mask
-    return dropped_output
-
-
-def forward_propagation(data, l1, l2, l3, l4):
-    a1, z1 = l1.forward_pass(data)
-    d1 = dropout_mask(a1, 0.7)
-    a2, z2 = l2.forward_pass(d1)
-    d2 = dropout_mask(a2, 0.7)
-    a3, z3 = l3.forward_pass(d2)
-    d3 = dropout_mask(a3, 0.7)
-    a4, z4 = l4.forward_pass(d3)
-    return z4, ((a1, z1), (a2, z2), (a3, z3), (a4, z4))
+def delta_calculation(intermediate_outputs, layers): ...
 
 
 def train():
-    X_train, Y_train, X_validation, Y_validation = load_data()
+    big_data = LoadData()
     layers = [
         Layer(he_normal(784, 512), np.zeros((512,)), "relu"),
         Layer(he_normal(512, 256), np.zeros((256,)), "relu"),
@@ -77,7 +87,6 @@ def train():
     max_epochs = 30
     correct_outputs = 0
     total_outputs = 0
-    cross_entropy_loss = lambda output, label: -np.log(output[label])
     calculate_dW = lambda delta, a_previous: delta * a_previous
     for epoch in range(max_epochs):
         X_train, Y_train = shuffle_data(X_train, Y_train)
@@ -88,14 +97,10 @@ def train():
             )
             correct_outputs += 1 if np.argmax(final_output) == correct_answer else 0
             total_outputs += 1
-            loss = cross_entropy_loss(final_output, correct_answer)
+            delta = delta_calculation(intermediate_outputs, layers)
             for layer_i in range(3, 0, -1):
                 layer = layers[layer_i]
-                if layer_i == 3:
-                    y = np.zeros((10,))
-                    y[correct_answer] = 1
-                    delta = final_output - y
-                delta = delta.reshape(delta.shape[0], 1)
+                layer_delta = delta[layer_i]
                 a_previous = intermediate_outputs[layer_i - 1][0].reshape(
                     1, intermediate_outputs[layer_i - 1][0].shape[0]
                 )
