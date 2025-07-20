1a460db39d41fbe951c45b4e8d19dffe3077afde
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-07-20 10:30:40 +0800

    Added in batching for training

--- Neural_Network.py
+++ Neural_Network.py
@@ -36,7 +36,7 @@ class Layer:
 
     def he_normal(self, fan_in, fan_out):
         return np.random.normal(
-            loc=0.0, scale=np.sqrt(2 / fan_in), size=(fan_out, fan_in)
+            loc=0.0, scale=np.sqrt(2 / fan_in), size=(fan_in, fan_out)
         )
 
 
@@ -48,6 +48,7 @@ class Network:
         self.l4 = Layer(128, 10)
         self.lr = 0.01
         self.weight_decay = 0.0001
+        self.batch_size = 64
 
     def dropout_mask(self, activated_output, keep_prob):
         mask = np.random.rand(*activated_output.shape) < keep_prob
@@ -55,16 +56,16 @@ class Network:
         return dropped_output
 
     def forward_propagation(self, data):
-        self.z1 = self.l1.weights @ data + self.l1.bias
+        self.z1 = data @ self.l1.weights + self.l1.bias
         self.a1 = self.ReLu(self.z1)
         self.d1 = self.dropout_mask(self.a1, 0.7)
-        self.z2 = self.l2.weights @ self.d1 + self.l2.bias
+        self.z2 = self.d1 @ self.l2.weights+ self.l2.bias
         self.a2 = self.ReLu(self.z2)
         self.d2 = self.dropout_mask(self.a2, 0.7)
-        self.z3 = self.l3.weights @ self.d2 + self.l3.bias
+        self.z3 = self.d2 @ self.l3.weights + self.l3.bias
         self.a3 = self.ReLu(self.z3)
         self.d3 = self.dropout_mask(self.a3, 0.7)
-        self.z4 = self.l4.weights @ self.d3 + self.l4.bias
+        self.z4 = self.d3 @ self.l4.weights + self.l4.bias
         self.a4 = self.softmax(self.z4)
 
     def calculate_gradients(self):
@@ -98,10 +99,11 @@ def train():
     total_outputs = 0
     for epoch in range(max_epochs):
         X_train, Y_train = big_data.load_training_data()
-        for row in range(len(X_train)):
-            small_data = X_train[row]
+        for row in range(0, len(X_train), nn.batch_size):
+            small_data = X_train[row: row + nn.batch_size]
+            correct_answer = Y_train[row: row + nn.batch_size]
             nn.forward_propagation(small_data)
-            nn.backward_propagation(Y_train[row])
+            nn.backward_propagation(correct_answer)
 
 
 train()
