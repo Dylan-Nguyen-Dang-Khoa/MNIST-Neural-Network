4e104b18b8f2e00c613c15e3192f203f71ee7709
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-08-01 23:10:57 +0800

    Added in visualisation of the images with the worst losses

--- visualise.py
+++ visualise.py
@@ -2,17 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Neural_Network import LoadData
 from random import randint
-
-
-def display_misclassified_digits():
-    big_data = LoadData(test_label=True)
-    X_test, Y_test = big_data.load_test_data()
-    predictions = np.loadtxt(
+big_data = LoadData(test_label=True)
+X_test, Y_test = big_data.load_test_data()
+predictions = np.loadtxt(
         "./Models/0.97814/Model Results/MNIST Kaggle Test Set/submissions.csv",
         delimiter=",",
         skiprows=1,
-        dtype=int,
-    )
+    ) 
+def display_random_misclassified_digits():
     correct_mask = predictions[:, 1] == Y_test[:]
     wrong_predictions = predictions[~correct_mask, 1]
     wrong_images = X_test[~correct_mask, :]
@@ -42,5 +39,35 @@ def display_misclassified_digits():
     else:
         print("Everything correct! 100% model accuracy!")
 
+def display_worst_misclassified_digits():
+    correct_mask = predictions[:, 1] == Y_test[:]
+    wrong_predictions = predictions[~correct_mask, 1]
+    wrong_images = X_test[~correct_mask, :]
+    wrong_images_true_label = Y_test[~correct_mask]
+    wrong_image_losses = predictions[~correct_mask, 2]
+    sorted_indices = np.argsort(-wrong_image_losses)
+    wrong_predictions, wrong_images, wrong_images_true_label = (
+        wrong_predictions[sorted_indices],
+        wrong_images[sorted_indices],
+        wrong_images_true_label[sorted_indices],
+    )
+    if len(wrong_images):
+        plt.figure(figsize=(10, 10))
+        num_to_show = min(25, len(wrong_images))
+        for i in range(num_to_show):
+            try:
+                plt.subplot(5, 5, i + 1)
+                plt.imshow(wrong_images[i, :].reshape((28, 28)), cmap="binary")
+                plt.title(
+                    f"Pred: {int(wrong_predictions[i])}\nTrue: {int(wrong_images_true_label[i])}",
+                    fontsize=8,
+                )
+                plt.axis("off")
+            except IndexError:
+                pass
+        plt.tight_layout()
+        plt.show()
+    else:
+        print("Everything correct! 100% model accuracy!")
 
-display_misclassified_digits()
+display_worst_misclassified_digits()
