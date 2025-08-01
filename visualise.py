8c447c193ae877a73a4086676cdff571fa7527e6
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-08-01 11:45:06 +0800

    Added visualisation, with random image selection

--- visualise.py
+++ visualise.py
@@ -1,24 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from Neural_Network import LoadData 
+from Neural_Network import LoadData
 from random import randint
 
-big_data = LoadData(test_label=False)
-X_test = big_data.load_test_data()
-Y_test = np.loadtxt("./Models/0.97814/Model Results/MNIST Kaggle Test Set/answers.csv", delimiter=",", skiprows=1, dtype=int)
-predictions = np.loadtxt("./Models/0.97814/Model Results/MNIST Kaggle Test Set/submissions.csv", delimiter=",", skiprows=1)
-correct_mask = predictions[:, 1] == Y_test[:, 1]
-wrong_images = X_test[~correct_mask, 1:]
-print(Y_test[0, 1])
-image_index = randint(0, len(wrong_images)-1)
-if len(wrong_images):
-    plt.figure(figsize=(10, 10))
-    for i in range(25):
-        plt.subplot(5, 5, i+1)
-        plt.imshow(wrong_images[image_index + i].reshape((28, 28)), cmap="binary")
-        plt.title(f"Label: {Y_test[image_index + i, 1]}", fontsize=8)
-        plt.axis("off")
-    plt.tight_layout()
-    plt.show()
+
+def display_misclassified_digits():
+    big_data = LoadData(test_label=True)
+    X_test, Y_test = big_data.load_test_data()
+    predictions = np.loadtxt(
+        "./Models/0.97814/Model Results/MNIST Kaggle Test Set/submissions.csv",
+        delimiter=",",
+        skiprows=1,
+        dtype=int,
+    )
+    correct_mask = predictions[:, 1] == Y_test[:]
+    wrong_predictions = predictions[~correct_mask, 1]
+    wrong_images = X_test[~correct_mask, :]
+    wrong_images_true_label = Y_test[~correct_mask]
+    permuted_indices = np.random.permutation(wrong_predictions.shape[0])
+    wrong_predictions, wrong_images, wrong_images_true_label = (
+        wrong_predictions[permuted_indices],
+        wrong_images[permuted_indices],
+        wrong_images_true_label[permuted_indices],
+    )
+    if len(wrong_images):
+        plt.figure(figsize=(10, 10))
+        num_to_show = min(25, len(wrong_images))
+        for i in range(num_to_show):
+            try:
+                plt.subplot(5, 5, i + 1)
+                plt.imshow(wrong_images[i, :].reshape((28, 28)), cmap="binary")
+                plt.title(
+                    f"Pred: {wrong_predictions[i]}\nTrue: {wrong_images_true_label[i]}",
+                    fontsize=8,
+                )
+                plt.axis("off")
+            except IndexError:
+                pass
+        plt.tight_layout()
+        plt.show()
+    else:
+        print("Everything correct! 100% model accuracy!")
 
 
+display_misclassified_digits()
